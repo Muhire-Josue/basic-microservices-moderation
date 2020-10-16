f77b07f7adfc964a63d103f7dd294ad45450f6f8
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-10-16 10:10:51 +0200

    add comment moderation

--- index.js
+++ index.js
@@ -8,8 +8,22 @@ const app = express();
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
 
+    const { type, data } = req.body;
+
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved';
+        await axios.post('http://localhost:4005/events', {
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        });
+    }
+    res.send({});
 });
 
 app.listen(4003, () => console.log('Server Running on port 4003'));
